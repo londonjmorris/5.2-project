26bccb57ffdee69f117f437c8017bd1c47d3b486
London Morris <londonjmorris@gmail.com>
2017-06-20 17:05:36 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const data = require('./data.js');
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+app.use(express.static('public'));
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+
+
+app.get('/users', function (req, res){
+  res.render('data', data);
+});
+
+
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+});
