38fdda50c5bdc5e16679cd6057e579a5dcc760d5
London Morris <londonjmorris@gmail.com>
2017-06-20 20:01:35 -0400

    added dynamic link

--- app.js
+++ app.js
@@ -10,7 +10,21 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.get('/users', function (req, res){
+  // 'data' in the res.render statement below identifies the template name
+  // data in the res.render statement is pulling from this app.js file.
   res.render('data', data);
+});
+
+app.get('/users/:id', function (req, res){
+  var id = req.params.id;
+  var user = data.users.find(function(user){
+    return user.id == id;
+  });
+
+  console.log('user',user);
+
+  res.render('user',user);
+
 });
 
 app.listen(3000, function () {
