c2e8db1b3d18220079bbd0fe4aa68cc7dc5862a5
London Morris <londonjmorris@gmail.com>
2017-07-05 16:20:41 -0400

    modified user page

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const userController = require("./controllers/user");
+const path = require('path');
+
 const app = express();
 
-app.use('static', express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
